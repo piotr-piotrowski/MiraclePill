55dd3ffa8c84728f080d6280e3bcd15958285a9c
Piotr Piotrowski <piotr@piotrovski.com>
2017-03-09 15:02:19 +0100

    After lecture 46 - Exercise: Extending Miracle Pills

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -8,10 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    @IBOutlet weak var countryLbl: UILabel!
+    @IBOutlet weak var countryText: UITextField!
+    @IBOutlet weak var zipCodeLbl: UILabel!
+    @IBOutlet weak var zipCodeText: UITextField!
+    @IBOutlet weak var buyNowBtn: UIButton!
+    @IBOutlet weak var successIndicatorImg: UIImageView!
+    
+    let states = ["Alaska", "Arkansas", "Alabama", "California", "Maine", "New York"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        statePicker.dataSource = self
+        statePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -19,6 +32,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stateBtnPressed(_ sender: Any) {
+        statePicker.isHidden = false
+        countryLbl.isHidden = true
+        countryText.isHidden = true
+        zipCodeLbl.isHidden = true
+        zipCodeText.isHidden = true
+    }
 
+    @IBAction func buyNowBtnPressed(_ sender: Any) {
+        for view in self.view.subviews as [UIView] {
+            view.isHidden = true
+        }
+        successIndicatorImg.isHidden = false
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+        countryLbl.isHidden = false
+        countryText.isHidden = false
+        zipCodeLbl.isHidden = false
+        zipCodeText.isHidden = false
+    }
 }
 
